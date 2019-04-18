8f3b9643a1d29c2b138af83527eafb2a63931ebb
Gerold Kölle <gerold.koelle@chello.at>
2019-04-18 21:43:29 +0200

    Initial Commit

---
+++ Girlfriend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Girlfriend
+//
+//  Created by Gerold Kölle on 18.04.19.
+//  Copyright © 2019 Gerold Kölle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
